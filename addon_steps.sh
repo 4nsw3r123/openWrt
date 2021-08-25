15ae49365da699c1e3632c563708e299179b1a3e
Potat0 <i@potat0.cc>
2021-08-25 23:02:12 +0800

    首次提交

---
+++ addon_steps.sh
@@ -0,0 +1,11 @@
+cd openwrt
+
+sed -i 's/Os/O3/g' include/target.mk
+sed -i 's/O2/O3/g' ./rules.mk
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+mkdir -p package/base-files/files/etc/openclash/core
+cd package/base-files/files/etc/openclash/core
+curl -L https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz | tar zxf -
+chmod +x clash
+cd ../../../../../..
